b3360b03619e3e8f7ffabc3748e2cbe88f68d2a0
saqlinezaman <saqlinemoaj@gmail.com>
2025-04-06 03:30:16 +0600

    frontend template fixed

---
+++ src/components/list.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from 'vue';
+import tusk from './tusk.vue';
+defineProps(['tusks'])
+const newTusk= ref({
+    id:0,
+    name:'',
+    completed:false
+})
+function saveTusk(){
+
+}
+</script>
+<template>
+     <!-- Form to Add Task -->
+     <div class="flex justify-center mt-16">
+        <div class="bg-white p-6 rounded-lg shadow-md mb-8 w-full max-w-3/5">
+            <form>
+             <div class="mb-4">
+                 <label for="task" class="block text-lg font-medium text-gray-700">New Task</label>
+                <input type="text" id="task" name="task"
+                 class="mt-2 p-2 w-full border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+               placeholder="Enter your task here" required />
+            </div>
+            <button type="submit"
+            class="bg-blue-500 text-white p-2 rounded-lg hover:bg-blue-600 w-full">Add Task</button>
+    </form>
+    <tusk v-for="tusk in tusks" :key="tusk.id" :tusk="tusk"/>
+  </div>
+</div>
+
+    <!-- Form to Add Task end-->
+ 
+</template>
---
+++ src/components/tusk.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps(['tusk']);
+</script>
+
+<template>
+     <li class="flex items-center justify-between bg-white px-4 py-3 rounded-lg shadow-md  my-5">
+  <label class="flex items-center w-full">
+    <input type="checkbox" v-model="tusk.completed" class="h-5 w-5 text-blue-500 border-gray-300 rounded mr-3">
+    <span :class="tusk.completed ? 'text-gray-400' : 'text-gray-800'" class="text-base font-medium">
+      {{ tusk.name }}
+    </span>
+  </label>
+</li>
+
+</template>
+
+<style scoped>
+
+</style>
